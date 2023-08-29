332ea72091fad9f994ed6c32d8096462b3fa994d
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-08-29 14:21:45 +0530

    [Rushi] add : wishList migration add & creation of tabel in database

---
+++ BookStore.Order/Migrations/20230829084826_wishList.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Order.Migrations
+{
+    /// <inheritdoc />
+    public partial class wishList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "wishList",
+                columns: table => new
+                {
+                    WishListID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookID = table.Column<int>(type: "int", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_wishList", x => x.WishListID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "wishList");
+        }
+    }
+}
